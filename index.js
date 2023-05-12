dc079c14b60694ec8890223cb4b29db33fe00442
Matt Pierson <matt.pierson789@gmail.com>
2023-05-12 14:53:10 -0400

    Spotify updates

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
-
 if (process.env.NODE_ENV === "development") {
-    console.log("In development mode");
+    console.log('in development mode')
     require("dotenv").config();
-
 }
 
 const app = express();
@@ -15,20 +12,41 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
- 
-    if (req.query.url)  {
+    // api url format domain + path + ? + api_key=key + 
+    if (req.query.prompt) {
+        // let api_key = `?api_key=${process.env.API_KEY}`
+        // let api_path = req.query.api_path
+        // console.log(api_path)
+        let url = 'https://api.openai.com/v1/completions'
+        // let fetchUrl = `${url}${api_path}${api_key}`
+        // console.log(fetchUrl)
+        let prompt = req.query.prompt
 
-        axios.get(req.query.url)
-        .then(data => res.send(data.data))
+        
+        axios.post(url, {
 
-    }   else {
+            model: "text-davinci-002",
+            prompt: prompt,
+            temperature: .7,
+            max_tokens: 300,
 
-        res.send("Hello world")
+        }, {
+
+            headers: {
+            
+            "Authorization": `Bearer ${process.env.API_KEY}`,
+            "Content-Type": "application/json"
+                
+            }
+
+
+        })               
+            .then(resBody => res.send(resBody.data));
+    } else {
+        res.send("Hello World!");
     }
-    
-})
+});
 
 app.listen(5001, () => {
-    console.log("lisening on port 5001");
-    
+    console.log("Listening on PORT: 5001")
 })
